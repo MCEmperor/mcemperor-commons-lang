3eb763865ad73a8abcece25356bc701f9610e0cf
Maurits de Jong <mauritsdajong@gmail.com>
2018-06-16 21:17:44 +0200

    Added method to retrieve all valid indices in reversed order.

--- src/main/java/nl/mcemperor/commons/lang/stream/Streams.java
+++ src/main/java/nl/mcemperor/commons/lang/stream/Streams.java
@@ -25,7 +25,19 @@ public class Streams {
 	 * @return A Stream with the elements in reversed order.
 	 */
 	public static <T> Stream<T> reverse(List<T> list) {
-		return IntStream.rangeClosed(list.size() - 1, 0)
+		return reversedIndices(list)
 			.mapToObj(i -> list.get(i));
+	}
+	
+	/**
+	 * Returns a Stream with all valid indices of {@code list}, in reversed order.
+	 * 
+	 * @param <T> The type of elements of the list.
+	 * @param list The list to stream from.
+	 * @return A Stream consisting of the indices.
+	 */
+	public static <T> IntStream reversedIndices(List<T> list) {
+		return IntStream.range(0, list.size())
+			.map(i -> list.size() - 1 - i);
 	}
 }
