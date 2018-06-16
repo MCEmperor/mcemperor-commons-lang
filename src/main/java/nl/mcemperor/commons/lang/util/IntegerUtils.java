9c51c081ca9e6c3ce7da9be7b4df3b32c284876d
Maurits de Jong <mauritsdajong@gmail.com>
2018-06-16 15:36:57 +0200

    Added some methods to the IntegerUtils.

--- src/main/java/nl/mcemperor/commons/lang/util/IntegerUtils.java
+++ src/main/java/nl/mcemperor/commons/lang/util/IntegerUtils.java
@@ -1,5 +1,8 @@
 package nl.mcemperor.commons.lang.util;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * The IntegerUtils class contains a set of static methods which make calculating with and getting properties from
  * integers convenient.
@@ -10,6 +13,53 @@ package nl.mcemperor.commons.lang.util;
 public class IntegerUtils {
 	
 	private IntegerUtils() { }
+	
+	/**
+	 * Gets the width of the given long. The width is the number of digits of the given number. The additive inversion
+	 * sign (minus sign), if applicable, increases the width by 1.
+	 * @param value An long.
+	 * @return The width of the given long.
+	 */
+	public static int getWidth(long value) {
+		int i = (value < 0 ? 1 : 0);
+		while (value != 0) {
+			value /= 10;
+			i++;
+		}
+		return i;
+	}
+	
+	/**
+	 * Gets all digits of a number as an <code>List&lt;Integer&gt;</code>.
+	 * @param number The number to get the digits from.
+	 * @return An array with the digits.
+	 */
+	public static List<Integer> getDigits(int number) {
+		int width = IntegerUtils.getWidth(number);
+		List<Integer> digits = new ArrayList<>();
+		for (int i = 0; i < width; i++) {
+			int div = (int) Math.pow(10, width - (i + 1));
+			int digit = number / div;
+			digits.add(digit);
+			number -= (digit * div);
+		}
+		return digits;
+	}
+	
+	/**
+	 * Try to parse the given str as an integer. If it fails, then return the default value.
+	 * @param str The string to parse.
+	 * @param defaultValue The default value.
+	 * @return The new value of the integer.
+	 */
+	public static int getOrDefault(String str, int defaultValue) {
+		try {
+			return Integer.valueOf(str);
+		}
+		catch (NumberFormatException exc) {
+			return defaultValue;
+		}
+	}
 	
 	/**
 	 * Converts an int array to a byte array, typecasting each int to a byte.
