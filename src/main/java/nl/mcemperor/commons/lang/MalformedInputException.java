72add6b4e19764988ab7d587b7437e8412aef824
Maurits de Jong <mauritsdajong@gmail.com>
2018-06-16 15:31:05 +0200

    Added MalformedInputException class. The exception defined in this class should be thrown if an input to a parser or method parsing some stream, is invalid.

---
+++ src/main/java/nl/mcemperor/commons/lang/MalformedInputException.java
@@ -0,0 +1,18 @@
+package nl.mcemperor.commons.lang;
+
+/**
+ * TODO: Finish documentation.
+ */
+/**
+ * MalformedInputException class.
+ *
+ * @author mauri
+ * @version 0.0.0
+ * @since 2018-06-16
+ */
+public class MalformedInputException extends Exception {
+	
+	public MalformedInputException(String message) {
+		super(message);
+	}
+}
